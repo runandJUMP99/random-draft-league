f6dd3c82aeb6386c8d3e5a6b0cfbc1fae9a46aa8
Steven Velez <stevendvelez9@gmail.com>
2022-04-02 01:15:23 -0400

    filter submissions without names

--- src/components/SubmitSelections/SubmittedSelections/SubmittedSelections.jsx
+++ src/components/SubmitSelections/SubmittedSelections/SubmittedSelections.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useSelector} from "react-redux"; 
+import { useSelector } from "react-redux";
 
 import AddSelection from "../../AddSelection/AddSelection";
 import SubmittedSelection from "./SubmittedSelection/SubmittedSelection";
@@ -7,53 +7,73 @@ import GlobalLoader from "../../UI/GlobalLoader/GlobalLoader";
 
 import classes from "./SubmittedSelections.module.css";
 
-const SubmittedSelections = ({setModalContent, setShowModal, submittedSelections}) => {
-    const selectedSelections = submittedSelections.filter(selection => selection.isSelected);
-    const notSelectedSelections = submittedSelections.filter(selection => !selection.isSelected);
-    const token = useSelector(state => state.auth.token);
-    let count = 0;
+const SubmittedSelections = ({
+  setModalContent,
+  setShowModal,
+  submittedSelections,
+}) => {
+  const filteredSubmittedSelections = submittedSelections.filter(
+    selection => selection.name
+  );
+  const selectedSelections = filteredSubmittedSelections.filter(
+    selection => selection.isSelected
+  );
+  const notSelectedSelections = filteredSubmittedSelections.filter(
+    selection => !selection.isSelected
+  );
+  const token = useSelector(state => state.auth.token);
+  let count = 0;
 
-    selectedSelections.sort((a, b) => {
-        return a.name.localeCompare(b.name, 'en', {'sensitivity': 'base'});
-    });
+  selectedSelections.sort((a, b) => {
+    return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
+  });
 
-    notSelectedSelections.sort((a, b) => {
-        return a.name.localeCompare(b.name, 'en', {'sensitivity': 'base'});
-    });
+  notSelectedSelections.sort((a, b) => {
+    return a.name.localeCompare(b.name, "en", { sensitivity: "base" });
+  });
 
-    submittedSelections = notSelectedSelections.concat(selectedSelections);
-    
-    function handleClick(id) {
-        const selectedSubmission = submittedSelections.filter(selection => selection.id === id);
+  submittedSelections = notSelectedSelections.concat(selectedSelections);
 
-        if (!selectedSubmission[0].isSelected && token) {
-            setModalContent(
-                <AddSelection setShowModal={setShowModal} submittedSelection={selectedSubmission[0]} />
-            );
-    
-            setShowModal(true);
-        }
-    }
-
-    return (
-        <div className={classes.SubmittedSelections}>
-            {submittedSelections.length === 0 ? <GlobalLoader /> : submittedSelections.map(submittedSelection => {
-                count++;
-                return (
-                    <SubmittedSelection 
-                        key={submittedSelection.id} 
-                        count={count} 
-                        from={submittedSelection.from}
-                        id={submittedSelection.id}
-                        isSelected={submittedSelection.isSelected}
-                        name={submittedSelection.name} 
-                        onClick={handleClick}
-                        time={submittedSelection.time}
-                    />
-                )
-            })}
-        </div>
+  function handleClick(id) {
+    const selectedSubmission = submittedSelections.filter(
+      selection => selection.id === id
     );
-}
+
+    if (!selectedSubmission[0].isSelected && token) {
+      setModalContent(
+        <AddSelection
+          setShowModal={setShowModal}
+          submittedSelection={selectedSubmission[0]}
+        />
+      );
+
+      setShowModal(true);
+    }
+  }
+
+  return (
+    <div className={classes.SubmittedSelections}>
+      {submittedSelections.length === 0 ? (
+        <GlobalLoader />
+      ) : (
+        submittedSelections.map(submittedSelection => {
+          count++;
+          return (
+            <SubmittedSelection
+              key={submittedSelection.id}
+              count={count}
+              from={submittedSelection.from}
+              id={submittedSelection.id}
+              isSelected={submittedSelection.isSelected}
+              name={submittedSelection.name}
+              onClick={handleClick}
+              time={submittedSelection.time}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+};
 
 export default SubmittedSelections;
